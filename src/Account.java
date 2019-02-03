330fa0a7b591aa4a65d2b608eaba0a28b579d0d6
Roy Esibe <bichiroy50@gmail.com>
2019-02-03 23:14:53 +0200

    Accessor methods

---
+++ src/Account.java
@@ -0,0 +1,16 @@
+
+public class Account {
+    private String name; // Instance variable
+
+    //method to set the name of the object
+    public void setName(String name)
+    {
+        this.name = name; //store the name
+    }
+
+    public String getName()
+    {
+        return name; // return value of the name to caller
+    }
+
+}
