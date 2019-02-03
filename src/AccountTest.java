08ec3a579be5d8c7adec2fe9441faff267d20b06
Roy Esibe <bichiroy50@gmail.com>
2019-02-03 23:33:20 +0200

    Add AccountTest class

---
+++ src/AccountTest.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class AccountTest {
+    public static void main(String[] args)
+    {   // scanner object
+        Scanner input  = new Scanner(System.in);
+
+        // Account object
+        Account myAccount = new Account();
+
+        //display name
+        System.out.printf("Initial name is: %s%n%n", myAccount.getName());
+
+
+
+    }
+}
