7860256905a1a22a58391c8aa153e78f244e44f3
Roy Esibe <bichiroy50@gmail.com>
2019-02-04 17:51:31 +0200

    Implement swtich case to choose Bank transaction method

--- src/AccountTest.java
+++ src/AccountTest.java
@@ -4,20 +4,36 @@ import javax.swing.JOptionPane;
 public class AccountTest {
     public static void main(String[] args)
     {   // scanner object
-//        Scanner input  = new Scanner(System.in);
+        Scanner input  = new Scanner(System.in);
 
         // Account object
         Account account1 = new Account("Echo B", 50000);
 
+        System.out.println("Welcome to Echo-b Bank");
+        System.out.println("Enter \n 1 to check balance\n 2 to Deposit\n 3 to withdraw");
+        int choice = input.nextInt();
+
+        switch (choice)
+        {
+            case 1:
+                System.out.printf("Your balance is: $%.2f", account1.getBalance());
+                break;
+            case 2:
+                System.out.println("Enter Amount: ");
+                double depositAmount = input.nextDouble();
+                account1.deposit(depositAmount);
+                System.out.printf("New balance: $%.2f", account1.getBalance());
+                break;
+            case 3:
+                System.out.println("Enter Amount: ");
+                double withdrawal = input.nextDouble();
+                account1.withdraw(withdrawal);
+                System.out.printf("New balance: $%.2f", account1.getBalance());
+                break;
+        }
+
         //display name
-        System.out.printf("%s balance: $%.2f%n", account1.getName(), account1.getBalance());
-
-        // Prompt for a deposit
-   //     System.out.println("Enter deposit: ");
-     //   double depositAmount = input.nextDouble();
-       // account1.deposit(depositAmount);
-
-   //     System.out.printf("New account balance is: $%.2f", account1.getBalance());
+       // System.out.printf("%s balance: $%.2f%n", account1.getName(), account1.getBalance());
 
     }
 }
