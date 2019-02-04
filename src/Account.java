e5045410adc5601e1cf2eff75135ede419e1bcb5
Roy Esibe <bichiroy50@gmail.com>
2019-02-04 17:08:25 +0200

    Add withdraw method

--- src/Account.java
+++ src/Account.java
@@ -1,14 +1,35 @@
 
 public class Account {
     private String name; // Instance variable
+    private double balance;
 
 
     //constructor initialises name with parameter name //Automatic setter
-    public Account(String name)
+    public Account(String name, double balance)
     {
         this.name = name;
+        if (balance > 0.0)
+            this.balance = balance;
     }
+
     //method to set the name of the object
+    public void deposit(double depositAmount){
+        if (depositAmount > 0.0)
+            balance = balance + depositAmount;
+    }
+
+    //method to collect money from account
+    public void withdraw(double withdrawal){
+        if(withdrawal >= balance)
+            System.out.println("You don't have enough funds");
+        else
+            balance = balance - withdrawal;
+    }
+
+    public double getBalance(){
+        return balance;
+    }
+
     public void setName(String name)
     {
         this.name = name; //store the name
--- src/AccountTest.java
+++ src/AccountTest.java
@@ -1,14 +1,23 @@
 import java.util.Scanner;
+import javax.swing.JOptionPane;
 
 public class AccountTest {
     public static void main(String[] args)
     {   // scanner object
-        Scanner input  = new Scanner(System.in);
+//        Scanner input  = new Scanner(System.in);
 
         // Account object
-        Account account1 = new Account("Echo B");
+        Account account1 = new Account("Echo B", 50000);
 
         //display name
-        System.out.printf("Initial name is: %s%n", account1.getName());
+        System.out.printf("%s balance: $%.2f%n", account1.getName(), account1.getBalance());
+
+        // Prompt for a deposit
+   //     System.out.println("Enter deposit: ");
+     //   double depositAmount = input.nextDouble();
+       // account1.deposit(depositAmount);
+
+   //     System.out.printf("New account balance is: $%.2f", account1.getBalance());
+
     }
 }
