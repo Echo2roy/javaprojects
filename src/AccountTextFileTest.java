d8523fdfd9449f5df4da79eb086bf26ceef06bc2
Roy Esibe <bichiroy50@gmail.com>
2019-02-06 11:59:59 +0200

    Add AccountTestFile main  class

--- src/AccountTextFileTest.java
+++ src/AccountTextFileTest.java
@@ -1,4 +1,10 @@
-package PACKAGE_NAME;
-
 public class AccountTextFileTest {
+    public static void main(String[] args)
+    {
+        AccountTextFile application = new AccountTextFile();
+
+        application.openFile();
+        application.addRecords();
+        application.closeFile();
+    }
 }
